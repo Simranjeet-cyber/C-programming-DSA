ca131a42b1e3510c3c8f965cb038ffcf4f5d3e48
Simranjeet-cyber <simranjeet2023456@gmail.com>
2026-03-01 14:00:16 +0530

    Refactor linked list implementation with new features

--- linkedlist1.c
+++ linkedlist1.c
@@ -1,73 +1,149 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <stdlib.h> 
 struct node
 {
-    int data;
-    struct node *next;
-}; 
-
-
-// create node function
-struct node *create_node(int d)
-{
-    struct node *n;
-    n = (struct node *)malloc(sizeof(struct node));
-    n->data = d;
+ int data ; struct node * next ;
+} ; 
+struct node* createNode(int data) {
+struct node* n = (struct node*)malloc(sizeof(struct node));
+    n->data = data;
     n->next = NULL;
     return n;
 }
-
-// append at end
-void append(struct node **ptr, int data)
+/* adds a node at the end of a linked list */
+void append ( struct node **ptr, int num )
 {
-    struct node *n, *temp;
-    n = create_node(data);
+ struct node *temp,*n ;
+ n = createNode(num);
+ if ( *ptr == NULL ) /* if the list is empty, create first node */
+ {
+ *ptr = n ;
+ }
+ else
+ {
+ temp = *ptr ;
+ /* go to last node */
+ while ( temp -> next != NULL )
+    temp = temp -> next ;
+ /* add node at the end */
+ temp -> next = n ;
+ }
+} 
+/* adds a new node at the beginning of the linked list */
+void insertBeg ( struct node **ptr, int num )
+{
+ struct node *n ;
+ /* add new node */
+ n = createNode(num);
+ n -> next = *ptr ;
+ *ptr = n ;
+} 
+void display ( struct node *ptr )
+{
+ /* traverse the entire linked list */
+ while ( ptr != NULL )
+ {
+ printf ( "%d ", ptr -> data ) ;
+ ptr=ptr->next;
+ }
+ printf ( "\n" ) ;
+} 
+void deleteFirst(struct node** ptr) {
+    if (*ptr == NULL) {
+        printf("List is empty\n");
+       // return head;
+    }
+    struct node* temp = *ptr;
+    *ptr = temp->next;
+    free(temp);
+    //return head;
+}
+int count ( struct node * ptr )
+{
+ int c = 0 ;
+ /* traverse the entire linked list */ 
+ while ( ptr != NULL )
+ {
+ ptr = ptr -> next ;
+ c++ ;
+ }
+ return c ;
+} 
+void insertAfter(struct node** ptr, int loc, int val)
+{
+    struct node *n=createNode(val);
+    struct node *temp=*ptr;
+    int i=1;
+    while(i<loc&& temp!=NULL)
+    {
+       temp=temp->next;
+        i++;
+    }
+    n->next=temp->next;
+    temp->next=n;
+    
+}
 
-    if (*ptr == NULL)
-        *ptr = n;
+void deleteEnd(struct node** ptr) {
+  
+    struct node* temp = *ptr, *prev=*ptr;
+    if (temp->next == NULL) {
+        free(temp);
+        *ptr = NULL;
+        return;
+    }
+    while (temp->next != NULL) {
+        prev=temp;
+        temp = temp->next;
+    }
+    prev->next=NULL;
+    free(temp);
+}
+void deleteVal(struct node **ptr, int val)
+{
+    struct node* temp=*ptr, *prev=*ptr;
+    while(temp!=NULL)
+    {
+        if (temp->data==val)
+            break;
+        prev=temp;
+        temp=temp->next;
+    }
+    //when value to be deleted is of first node
+    if(temp==*ptr)
+    {
+        *ptr=(*ptr)->next;
+        free(temp);
+    }
     else
     {
-        temp = *ptr;
-        while (temp->next != NULL)
-            temp = temp->next;
-
-        temp->next = n;
+        prev->next=temp->next;
+        free(temp);
     }
 }
-
-// insert at beginning
-void insertBeg(struct node **ptr, int data)
+int main( )
 {
-    struct node *n;
-    n = create_node(data);
-    n->next = *ptr;
-    *ptr = n;
-}
-
-// display list
-void display(struct node *start)
-{
-    while (start != NULL)
-    {
-        printf("%d -> ", start->data);
-        start = start->next;
-    }
-    printf("NULL");
-}
-
-int main()
-{
-    struct node *start = NULL;
-
-    append(&start, 20);
-    append(&start, 30);
-    append(&start, 50);
-
-    insertBeg(&start, 24);
-//here some display issue is there
-    display(start);
-
-    return 0;
-
+ struct node *start ;
+ start = NULL ; 
+ printf ( "No. of elements in the Linked List = %d\n", count ( start ) ) ;
+ append ( &start, 14 ) ; 
+ append ( &start, 30 ) ; 
+ append ( &start, 25 ) ;
+ append ( &start, 42 ) ;
+ append ( &start, 17 ) ;
+ display ( start) ;
+ insertBeg ( &start, 99 ) ; 
+ insertBeg ( &start, 88 ) ; 
+ insertBeg ( &start, 77 ) ;
+ display ( start ) ;
+ insertAfter(&start,3,26);
+ display ( start ) ;
+  printf ( "No. of elements in the Linked List = %d\n", count ( start ) ) ;
+ deleteFirst(&start);
+ display ( start ) ;
+ deleteEnd(&start);
+ display ( start ) ;
+ deleteVal(&start,99);
+ display ( start ) ;
+ return 0;
 }
